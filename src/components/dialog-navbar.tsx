9c2d9d995152cdddd8f54294ad2d96ba7e8e2a88
Mr uprizing <uprizingfaze@gmail.com>
2025-11-24 11:33:54 -0500

    ward

---
+++ src/components/dialog-navbar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import * as React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+export function DialogNavbar() {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <span className="flex items-center font-mono text-muted-foreground text-xs sm:-mx-2">
+        Press{" "}
+        <kbd className="bg-card text-muted-foreground pointer-events-none inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 select-none ml-1">
+          <span className="text-xs text-muted-foreground">⌘</span>K
+        </kbd>
+      </span>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import cn from "clsx";
+import { usePathname } from "next/navigation";
+import { Link } from "next-view-transitions";
+import { DialogNavbar } from "./dialog-navbar";
+
+function Item(props: React.ComponentProps<typeof Link>) {
+  const pathname = usePathname();
+  const href = props.href;
+
+  if (typeof href !== "string") {
+    throw new Error("`href` must be a string");
+  }
+
+  const isActive = pathname === href || pathname.startsWith(`${href}/`);
+
+  return (
+    <li
+      className={cn(
+        isActive
+          ? "text-primary"
+          : "text-muted-foreground hover:text-foreground",
+        "transition-colors",
+        "sm:-mx-3",
+      )}
+    >
+      <Link
+        {...props}
+        className="inline-block w-full px-2 font-mono text-right text-sm font-med"
+        draggable={false}
+        prefetch={true}
+      />
+    </li>
+  );
+}
+
+export default function Navbar() {
+  return (
+    <nav className="sm:mr-6 md:mr-10 lg:mr-14 w-full sm:w-16">
+      <ul className=" text-right sm:sticky top-6 md:top-10 lg:top-14 mb-6 sm:mb-0 flex gap-2 justify-end sm:block">
+        <Item href="/">About</Item>
+        <Item href="/skills">Skills</Item>
+        <Item href="/thoughts">Thoughts</Item>
+        <Item href="/projects">Projects</Item>
+        <hr className="border-t border-dashed border my-4 sm:-mx-1" />
+        <DialogNavbar />
+      </ul>
+    </nav>
+  );
+}
